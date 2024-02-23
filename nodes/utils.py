c7c865fc39fcc4c588af55eb1c8961b14ea6a889
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2024-02-23 22:07:48 +0400

    clean name

--- nodes/utils.py
+++ nodes/utils.py
@@ -5,7 +5,7 @@ def get_video_metadata(video_path):
     return reader.get_meta_data()
 
 def clean_string(input_string):
-    invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+    invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|',"#"," ","!"]
     cleaned_string = ''.join(c if c not in invalid_chars else '_' for c in input_string)
     cleaned_string = cleaned_string.strip(" .")
     return cleaned_string
